54ddcf8ea34c27ef5a5c8a37ceaf2f3380109cf2
JD <113867662+DYA00@users.noreply.github.com>
2026-01-17 19:22:19 +0900

    Implement mySqrt function for Sqrt(x) problem

---
+++ java/easy/Sqrt(x).java
@@ -0,0 +1,17 @@
+// Solution for the problem Sqrt(x) in leetcode.
+class Solution {
+    public int mySqrt(int x) {
+        int num = 1;
+
+        while (x / num > num) {
+            num ++;
+        }
+
+        if (x / num == num) {
+            return num;
+        }
+        else {
+            return num - 1;
+        }
+    }
+}

aa8e260e05659fc0592bde034b4a4b05321d8ca1
JD <113867662+DYA00@users.noreply.github.com>
2026-03-13 16:24:32 +0900

    Implement palindrome check in ValidPalindrome class

---
+++ java/easy/125_ValidPalindrome.java
@@ -0,0 +1,24 @@
+//Description for the problem, Valid Plaindrome in the leetcode.
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        if (s.length() <= 1) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left ++;
+            right --;
+        }
+
+        return true;
+    }
+}

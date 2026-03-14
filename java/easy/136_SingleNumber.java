b27e9397aaefe76ee75f07dfc9f18d8f8f1ad3cb
JD <113867662+DYA00@users.noreply.github.com>
2026-03-14 13:34:18 +0900

    Add solution for Single Number problem
    Implement solution for the Single Number problem.

---
+++ java/easy/136_SingleNumber.java
@@ -0,0 +1,12 @@
+//Solution for the problem, Single Number in leetcode.
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+}

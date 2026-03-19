4142d244f62f2b6d3e90d3c6e3fd7e7495b7512f
JD <113867662+DYA00@users.noreply.github.com>
2026-03-19 12:07:27 +0900

    Add LinkedListCycle detection solution
    Implement solution to detect cycle in a linked list.

---
+++ java/easy/141_LinkedListCycle.java
@@ -0,0 +1,30 @@
+//Solution for the problem, linked list cycle, asking to determine the existence of loop in the linked cycle in the leetcode.
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            head = head.next;
+            fast = fast.next.next;
+            if (head == fast) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+}

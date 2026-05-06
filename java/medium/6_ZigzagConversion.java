9514328e5dd23bb42bac2ea8890a9b9740a562e5
JD <113867662+DYA00@users.noreply.github.com>
2026-05-06 19:43:55 +0900

    Add ZigzagConversion solution in Java
    Implement the Zigzag Conversion algorithm for strings.

---
+++ java/medium/6_ZigzagConversion.java
@@ -0,0 +1,34 @@
+//Solution for the problem, zigzag conversion in the leetcode.
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int curr = 0;
+        boolean down = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curr].append(c);
+
+            if (curr == 0  || curr == numRows - 1) {
+                down = !down;
+            }
+
+            curr += down ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}

8439027132e015c34ca7ea7dd956eca7f699390c
JD <113867662+DYA00@users.noreply.github.com>
2026-05-19 15:33:07 +0900

    Add solution for counting complete tree nodes
    Implement solution to count nodes in a complete binary tree.

---
+++ java/easy/222_CountCompleteTreeNodes.java
@@ -0,0 +1,43 @@
+//Solution for the problem, Count Complete Tree Nodes in the leetcode.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+
+        int leftHeight = getLeftHeight(root.left);
+        int rightHeight = getLeftHeight(root.right);
+
+        if (leftHeight == rightHeight) {
+            return ((1 << leftHeight) + countNodes(root.right));
+        }
+        else {
+            return ((1 << rightHeight) + countNodes(root.left));
+        }
+    }
+
+    private int getLeftHeight(TreeNode root) {
+        int height = 0;
+
+        while (root != null) {
+            height++;
+            root = root.left;
+        }
+
+        return height;
+    }
+}

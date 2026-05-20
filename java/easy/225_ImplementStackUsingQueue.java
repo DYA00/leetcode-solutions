5175a510aada1c635531b2fbf4412157e7731c66
JD <113867662+DYA00@users.noreply.github.com>
2026-05-20 20:24:43 +0900

    Add MyStack implementation using two queues
    Implemented a stack using two queues with push, pop, top, and empty methods.

---
+++ java/easy/225_ImplementStackUsingQueue.java
@@ -0,0 +1,54 @@
+//Solution for the problem, Implement Stack Using Queue.
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q1.offer(x);
+    }
+    
+    public int pop() {
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+        int last = q1.poll();
+        
+        Queue<Integer> temp = new LinkedList<>();
+        q1 = q2;
+        q2 = temp;
+
+        return last;
+    }
+    
+    public int top() {
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+        int last = q1.poll();
+        q2.offer(last);
+        
+        Queue<Integer> temp = new LinkedList<>();
+        q1 = q2;
+        q2 = temp;
+
+        return last;
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

66154a9deaa0c2eebc9556a9355f52fdc8959d33
JD <113867662+DYA00@users.noreply.github.com>
2026-06-29 20:51:52 +0900

    Add solution for Valid Anagram problem
    Implement solution for Valid Anagram problem.

---
+++ java/easy/242_ValidAnagram.java
@@ -0,0 +1,12 @@
+//Solution for the problem, Valid Anagram in the leetcode.
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s_chars = s.toCharArray();
+        char[] t_chars = t.toCharArray();
+
+        Arrays.sort(s_chars);
+        Arrays.sort(t_chars);
+
+        return Arrays.equals(s_chars, t_chars);
+    }
+}
